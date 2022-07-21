511c19f9afd945663bb7965926b79e74c8976336
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-07-21 19:56:26 +0530

    Calculate Total Fare Of Journey

---
+++ CabInvoiceGenerator/CabInvoiceGeneratorTest/CabInvoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CabInvoiceGeneratorTest
+{
+    internal class CabInvoice
+    {
+        public CabInvoice()
+        {
+        }
+
+        internal int CalculateFare(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
